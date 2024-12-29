c0ade9e14c5f0a1b388cb26da04454c2bfc30173
Sonam Gautam <gautamsonam011@gmail.com>
2024-12-29 20:52:55 +0530

    c

---
+++ .history/ecommerce-frontend/src/App_20241229203335.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
+
+function App() {
+  return (
+   <>
+   <div>
+    <Navbar/>
+    <Container>
+
+    </Container>
+    <Footer/>
+   </div>
+   </>
+  )
+}
+
+export default App
